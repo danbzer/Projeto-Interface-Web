1105133e96e86724fb996d8f460e98f74eaefb64
kagunkarla <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>
2026-05-19 17:50:45 -0300

    bookPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import LoginForm from './pages/LoginForm'
+import BookPage from './pages/bookPage/BookPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/entrar" element={<LoginForm />} />
+      <Route path="/livro" element={<BookPage />} />
     </Routes>
   )
 }
